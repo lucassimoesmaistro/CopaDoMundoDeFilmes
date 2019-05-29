d13e09f940e215972b152016e8a4f49d29fd79d0
Lucas Simoes Maistro <lucas.maistro.ext@extrafarma.com.br>
2019-05-29 17:21:09 -0300

    Arquitetura

---
+++ CopaDoMundoDeFilmes/CopaDoMundoDeFilmes.Core.Dominio/Modelos/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopaDoMundoDeFilmes.Core.Dominio.Modelos
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int Ano { get; set; }
+        public decimal Nota { get; set; }
+
+    }
+}
---
+++ CopaDoMundoDeFilmes/CopaDoMundoDeFilmes.Testes/UnitTestServicoDeAplicacao.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CopaDoMundoDeFilmes.Testes
+{
+    public class UnitTestServicoDeAplicacao
+    {
+        [Fact]
+        public void DeveRetornarListaDeFilmes()
+        {
+
+        }
+    }
+}
