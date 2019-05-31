968b3752e9e6667c59935d34c3a874c6fee385e4
Lucas Simões <Lucassimoesmaistro@gmail.com>
2019-05-30 23:15:28 -0300

    Jogos

---
+++ CopaDoMundoDeFilmes/CopaDoMundoDeFilmes.Core.Dominio/Modelos/Jogo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopaDoMundoDeFilmes.Core.Dominio.Modelos
+{
+    public class Jogo
+    {
+        public int Numero { get; set; }
+        public Filme Filme1 { get; set; }
+        public Filme Filme2 { get; set; }
+    }
+}
---
+++ CopaDoMundoDeFilmes/CopaDoMundoDeFilmes.Core.Dominio/Servicos/Campeonato.cs
@@ -0,0 +1,31 @@
+﻿using CopaDoMundoDeFilmes.Core.Dominio.Interfaces;
+using CopaDoMundoDeFilmes.Core.Dominio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopaDoMundoDeFilmes.Core.Dominio.Servicos
+{
+    public class Campeonato : ICampeonato
+    {
+        public List<Filme> GerarCampeonato(List<Filme> filmesSelecionados)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Filme> ObterFilmes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Filme DefineVencedor(Filme filme1, Filme filme2)
+        {
+            return filme1.Nota > filme2.Nota ? filme1 : filme2;
+        }
+
+        public List<Jogo> MontarJogos(int quantidadeDeJogos, List<Filme> listaDeFilmes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
